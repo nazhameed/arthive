370786dcd5568990d962ebd0325a8e5d18ef8723
nazhameed <naz.hameed@sky.com>
2025-09-30 13:15:58 +0100

    add login and register to index page. add dashboard

--- gallery/views.py
+++ gallery/views.py
@@ -1,20 +1,50 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
-from django.http import HttpResponse
-# Create your views here.
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            login(request, user)
-            return redirect('index')
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
-    return HttpResponse("infinite robux By Salah, 7<br>make me the best player at 99 nights in the forest by Inaayah, 5")
+    # Show landing page with login/register forms if not logged in
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    login_form = AuthenticationForm()
+    signup_form = SignUpForm()
+    login_error = None
+    signup_error = None
+    # Handle registration POST
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            signup_form = SignUpForm(request.POST)
+            if signup_form.is_valid():
+                user = signup_form.save(commit=False)
+                user.set_password(signup_form.cleaned_data['password'])
+                user.save()
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                signup_error = signup_form.errors
+        elif 'login' in request.POST:
+            login_form = AuthenticationForm(request, data=request.POST)
+            if login_form.is_valid():
+                user = authenticate(
+                    username=login_form.cleaned_data['username'],
+                    password=login_form.cleaned_data['password']
+                )
+                if user is not None:
+                    login(request, user)
+                    return redirect('dashboard')
+                else:
+                    login_error = 'Invalid username or password.'
+            else:
+                login_error = login_form.errors
+    return render(request, 'index.html', {
+        'form': login_form,
+        'signup_form': signup_form,
+        'login_error': login_error,
+        'signup_error': signup_error
+    })
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
