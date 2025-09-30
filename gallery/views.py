8df7ced96a33f3e011c20ab7f773ef6ccb779497
nazhameed <naz.hameed@sky.com>
2025-09-30 11:00:20 +0100

    create sign up page

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 from django.http import HttpResponse
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
 def index(request):
     return HttpResponse("infinite robux By Salah, 7<br>make me the best player at 99 nights in the forest by Inaayah, 5")
