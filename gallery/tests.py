bebcac944806171c7462e14d546fa7d73b8c513e
nazhameed <naz.hameed@sky.com>
2025-10-13 11:35:14 +0100

    Add basic Child model test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Child
 
-# Create your tests here.
+class ChildModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.child = Child.objects.create(name='Alice', age=7, parent=self.user)
+
+    def test_child_str(self):
+        self.assertEqual(str(self.child), 'Alice')
